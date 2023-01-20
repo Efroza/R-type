f181ab99bcbfbe1ba1d6aeca9e37e2601befdd2c
Brice Boualavong <brice.boualavong@epitech.eu>
2023-01-20 01:16:14 +0100

    [ADD] two tests folders client and server each of them having a main.cpp to see if the two binaries are built with the Makefile that was wreate by CMake

---
+++ client/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-PAR-5-2-rtype-luc1.schmitt
+** File description:
+** main
+*/
+
+#include "iostream"
+#include "stdio.h"
+
+int main()
+{
+    std::cout << "Hello World client" << std::endl;
+    return 0;
+}
---
+++ server/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-PAR-5-2-rtype-luc1.schmitt
+** File description:
+** main
+*/
+
+#include "iostream"
+
+int main()
+{
+    std::cout << "Hello World server" << std::endl;
+    return 0;
+}
