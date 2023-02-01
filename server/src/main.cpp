886aba7da54008816727f9252e075cd1411ae03d
Lucsc <luc1.schmitt@epitech.eu>
2023-02-01 22:17:15 +0100

    [ADD] start std::vector the receive thread get the std::vector but not the send_thread

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -8,42 +8,68 @@
 #include <iostream>
 #include <asio.hpp>
 #include <thread>
+#include <vector>
 
 using namespace asio;
 
 const int BUFSIZE = 1024;
-const std::string HOST = "127.0.0.1";
-const int PORT = 12345;
+const std::string HOST = "127.0.0.1"; // Localhost : 127.0.0.1
+const int PORT = 12345; // Port to listen on
 
 char buf[BUFSIZE];
 
-void receive_thread(ip::udp::socket& socket, ip::udp::endpoint& remote) {
-    while (true) {
+void receive_thread(ip::udp::socket& socket, ip::udp::endpoint& remote, std::vector<ip::udp::endpoint>& endpoints) { // Receive data from clients
+    while (true) { // Loop forever thread will receive data from clients
         size_t len = socket.receive_from(buffer(buf, BUFSIZE), remote);
-        std::cout << "Received data from " << remote.address().to_string() << ":" << remote.port() << ": ";
+
+        if (std::find(endpoints.begin(), endpoints.end(), remote) == endpoints.end()) {
+            endpoints.push_back(remote);
+            std::cout << "New client connected: " << remote.address().to_string() << ":" << remote.port() << ". Id = " << endpoints.size() << "." << std::endl;
+        }
+        std::cout << "Received data from client " << std::find(endpoints.begin(), endpoints.end(), remote) - endpoints.begin() + 1 << ": ";
         std::cout.write(buf, len) << std::endl;
     }
 }
 
-void send_thread(ip::udp::socket& socket, ip::udp::endpoint& remote) {
-    while (true) {
+void send_thread(ip::udp::socket& socket, ip::udp::endpoint& remote, std::vector<ip::udp::endpoint>& endpoints) { // Send data to clients
+    
+    if (endpoints.empty()) {
+            std::cout << "No clients connected." << std::endl;
+            while (true)
+                if (endpoints.size() > 0) {
+                    std::cout << "A client has connected to the server" << std::endl;
+                    std::cout << "Number of clients : " << endpoints.size() << std::endl;
+                    break;
+                }
+    }
+    while (true) { // Loop forever thread will send data to clients
         std::cout << "Enter message to send: ";
         std::string message;
         std::getline(std::cin, message);
-        socket.send_to(buffer(message), remote);
+        if (message[0] == '1') {
+            std::cout << "Sending data to every client" << std::endl;
+            std::cout << endpoints.size() << std::endl;
+            for (auto& endpoint : endpoints) {
+                socket.send_to(buffer(message), endpoint);
+            }
+        } else {
+            socket.send_to(buffer(message), remote);
+        }
     }
 }
 
 int main() {
     io_service io_service;
-    ip::udp::socket socket(io_service, ip::udp::endpoint(ip::udp::v4(), PORT));
-    ip::udp::endpoint remote(ip::address::from_string(HOST), PORT);
+    ip::udp::socket socket(io_service, ip::udp::endpoint(ip::udp::v4(), PORT)); // Bind to port
+    ip::udp::endpoint remote(ip::address::from_string(HOST), PORT); // Remote endpoint
+    // std::vector<ip::udp::endpoint> endpoints;
+    std::vector<ip::udp::endpoint> endpoints;
 
-    std::thread receive(receive_thread, std::ref(socket), std::ref(remote));
-    std::thread send(send_thread, std::ref(socket), std::ref(remote));
+    std::thread receive(receive_thread, std::ref(socket), std::ref(remote), std::ref(endpoints)); // Create thread to receive data from clients
+    std::thread send(send_thread, std::ref(socket), std::ref(remote), std::ref(endpoints)); // Create thread to send data to clients
 
-    receive.join();
-    send.join();
+    receive.join(); // Wait for threads 'receive' to finish
+    send.join(); // Wait for threads 'send' to finish
 
     return 0;
 }
