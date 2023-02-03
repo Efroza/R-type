592f99131acaf499863f0668b63318446329e3af
Yohan <yohan.henry@epitech.eu>
2023-02-03 17:17:39 +0100

    [ADD] network.hpp network.cpp network class for handling network connexion

---
+++ Engine/Network/network.cpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** Engine
+** File description:
+** network
+*/
+
+#include "network.hpp"
+
+network::network(const std::string &ip_address, size_t port)
+{
+}
+
+network::~network()
+{
+}
+
+void network::connexion_network()
+{
+}
