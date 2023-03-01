c6c6cac76860b0d23d5ced4c8ec2ce8f2b0e894c
Royale BADIABO <royale.badiabo@epitech.eu>
2023-03-01 12:18:35 +0100

    [ADD] the second part of the doxy

--- Engine/ComponentSystem/draw.cpp
+++ Engine/ComponentSystem/draw.cpp
@@ -12,6 +12,10 @@
 #include "rect.hpp"
 #include <SFML/Graphics.hpp>
 
+/**
+ * @file draw.cpp
+ */
+
 componentSystem::draw::draw()
 {
 }
@@ -20,10 +24,26 @@ componentSystem::draw::~draw()
 {
 }
 
+/**
+ * @brief Factory function for creating an instance of the draw component system.
+ *
+ * @return A pointer to a new instance of the draw component system.
+ * @details The function is declared as extern "C", which specifies that it has C linkage (i.e., it can be called from C code).
+ * @details This is necessary because the component system is loaded as a shared library.
+ */
+
 extern "C" IComponentSystem *createComponentSystem()
 {
     return new componentSystem::draw;
 }
+
+/**
+ * @brief Will load the texture into the data object and associate it with the given id if it is not already loaded.
+ *
+ * @param id The id of the texture.
+ * @param db Represent the data object for storing the image data.
+ * @param file The file path of the image.
+ */
 
 static void load_texture(std::uint8_t id, data *db, const std::string &file)
 {
@@ -33,6 +53,19 @@ static void load_texture(std::uint8_t id, data *db, const std::string &file)
     texture.setSmooth(true);
     db->insert_data<sf::Texture>(std::move(texture), id);
 }
+
+/**
+ * @brief Updates the draw component system by processing all relevant image, position, and rectangle components.
+ *
+ * @param reg The registry containing all entities and their associated components.
+ * @param images The sparse array of image components.
+ * @param positions The sparse array of position components.
+ * @param rects The sparse array of rectangle components.
+ * @param interactions The sparse array of interaction components.
+ * @param db Represent the data object for storing the image data.
+ * @param window The window in which the image will be drawn.
+ * @details The function will draw all images in the window.
+ */
 
 static void create_sprite(std::uint8_t id, data *db, component::image &image, component::position &position)
 {
@@ -49,6 +82,14 @@ static void create_sprite(std::uint8_t id, data *db, component::image &image, co
     std::cout << "texture added: " << image.name_file << std::endl;
 }
 
+/**
+ * @brief Will manage the event of the window like (close, key pressed, mouse pressed, etc...)
+ *
+ * @param event SFML variable for managing all event from the window
+ * @param window Where the event will be managed (SFML), where the image will be drawn
+ * @param interactions The sparse array of interaction components like (up, down, left, right, etc...)
+ */
+
 static void handle_event(sf::Event &event, sf::RenderWindow &window, sparse_array<component::interaction> &interactions)
 {
     while (window.pollEvent(event)) {
@@ -63,6 +104,15 @@ static void handle_event(sf::Event &event, sf::RenderWindow &window, sparse_arra
         }
     }
 }
+
+/**
+ * @brief Draws images on the screen according to their positions and draw properties.
+ *
+ * @param reg The registry containing all entities and their associated components.
+ * @param images The sparse array of image components to draw.
+ * @param positions The sparse array of position components associated with the images.
+ * @param draw The sparse array of draw components associated with the images.
+ */
 
 void draw_system(registry &reg
 , sparse_array<component::image> &images
@@ -102,6 +152,14 @@ void draw_system(registry &reg
     }
 }
 
+/**
+ * @brief Create a rect object
+ *
+ * @param id Record the id of the rect object to recover it later
+ * @param db Where the rect will be stored
+ * @param rect Component that will help to create the rect object in SFML
+ */
+
 static void create_rect(std::uint32_t id, data *db, component::rect &rect)
 {
     if (db == nullptr || db->data_exist<sf::Texture>(id) == false)
@@ -119,6 +177,15 @@ static void create_rect(std::uint32_t id, data *db, component::rect &rect)
     db->insert_data<sf::IntRect>(std::move(rect_sfml), id);
 }
 
+/**
+ * @brief Update the rect object
+ *
+ * @param id Record the id of the rect object to recover it later
+ * @param db Where the rect will be stored
+ * @param rect Component that will help to update the rect object in SFML
+ * @details The function will update the rect object in SFML, to check is state and update it.
+ */
+
 static void update_rect(std::uint32_t id, data *db, component::rect &rect)
 {
     if (db == nullptr || db->data_exist<sf::IntRect>(id) == false)
@@ -130,6 +197,15 @@ static void update_rect(std::uint32_t id, data *db, component::rect &rect)
     rect_sfml.height = rect.height;
 }
 
+/**
+ * @brief Load the rect object
+ *
+ * @param id Record the id of the rect object to recover it later
+ * @param db Where the rect is stored
+ * @param rect Component that will help to load the rect object in SFML
+ * @details The function will load the rect object in SFML.
+ */
+
 static void load_rect(std::uint32_t id, data *db, component::rect &rect)
 {
     if (db == nullptr || db->data_exist<sf::Sprite>(id) == false || db->data_exist<sf::IntRect>(id) == false)
@@ -138,6 +214,19 @@ static void load_rect(std::uint32_t id, data *db, component::rect &rect)
     sf::IntRect &rect_sfml = db->get_data<sf::IntRect>(id);
     sprite.setTextureRect(rect_sfml);
 }
+
+/**
+  * @brief System responsible for updating and drawing rectangular shapes.
+  *
+  * @param reg A reference to the entity-component system registry.
+  * @param rects A sparse array of components that contain the rectangular shape properties for each entity.
+  * @param images A sparse array of components that contain the image properties for each entity.
+  * @param draw A sparse array of components that determine whether an entity should be drawn.
+  * @details This system iterates through the sparse arrays of components (images, rects, and draw)
+  * @details and for each entity with all three components, it checks if the image has a valid
+  * @details database pointer. If so, it checks if the entity has a valid sf::IntRect object in
+  * @details the database. If not, it creates one, otherwise it updates and loads the existing one.
+  */
 
 void draw_rect_system(registry &reg
 , sparse_array<component::rect> &rects
